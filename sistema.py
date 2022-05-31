876ce697039a3c0db49f77ef2c09e1861928006a
Charles Eduardo <charleseduardofaria@gmail.com>
2022-05-31 19:13:41 -0300

    primeiro-commit

---
+++ sistema.py
@@ -0,0 +1,26 @@
+def create():
+    pass
+
+def search():
+    pass
+
+def update():
+    pass
+
+def delete():
+    pass
+
+def read():
+    pass
+
+print("-------------------------------")
+print("---------  Sistema X  ---------")
+print("-------------------------------")
+print("1 - Cadastrar")
+print("2 - Pesquisar")
+print("3 - Atualizar")
+print("4 - Apagar")
+print("5 - Listar todos")
+print("0 - Sair")
+
+op = int(input("\nDigite a opção: "))
