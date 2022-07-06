424a6d0722bab755672ab2e2c04fbed1914c4ba4
Charles Eduardo <charleseduardofaria@gmail.com>
2022-07-06 17:00:06 -0300

    CRUD de funcionario e hospede completo
    Co-authored-by: Halyson <HalysonSA@users.noreply.github.com>

--- sistema.py
+++ sistema.py
@@ -1,7 +1,61 @@
 import os
 from datetime import date
 
-funcionario = {}
+funcionario = {
+    "1": {
+        "nome": "João",
+        "cpf": "1",
+        "salario": 1000,
+        "ocupacao": "Gerente"
+    },
+    "2": {
+        "nome": "Maria",
+        "cpf": "2",
+        "salario": 2000,
+        "ocupacao": "Gerente"
+    }
+}
+
+hospede = {
+     "1": {
+        "nome": "João",
+        "cpf": "1",
+        "email": "dasdasd",
+        "endereco": "asdasd",
+        "telefone": "asdasd"
+    },
+    "2": {
+        "nome": "Maria",
+        "cpf": "2",
+        "email": "dasdasd",
+        "endereco": "asdasd",
+        "telefone": "asdasd"
+    }
+}
+''' 
+reservas = {
+    "apt":{
+        "classe":"standard",
+        "ocupado":False,
+        "data_entrada":date.today(),
+        "data_saida":date.today(),
+        "hospede": hospede['cpf']
+        tentar dizer qual hospede está no quarto
+
+
+    }
+}
+'''
+
+#func ={ 
+    #cpf:{
+        #"nome": nome,
+        #"cpf": cpf,
+        #"salario": salario,
+    #}
+#}
+
+
 
 def menu_main():
     print("--------------------------------------------")
@@ -26,7 +80,8 @@ def menu_hospede():
     print('1 - Cadastrar hóspedes')
     print('2 - Atualizar hóspedes')
     print("3 - Consultar hóspedes")
-    print("4 - Deletar hóspedes")
+    print("4 - Pesquisar hóspedes")
+    print("5 - Deletar hóspedes")
     print("0 - Voltar")
 
     op = input("\nDigite a opção: ")
@@ -41,8 +96,9 @@ def menu_funcionario():
     print("---------------------------------")
     print('1 - Cadastrar funcionário')
     print('2 - Atualizar funcionário')
-    print("3 - Consultar funcionário")
-    print("4 - Deletar funcionário")
+    print("3 - Listar funcionário")
+    print("4 - Pesquisar funcionário")
+    print("5 - Deletar funcionário")
     print("0 - Voltar")
         
     op = input("\nDigite a opção: ")
@@ -79,11 +135,114 @@ def menu_check():
 
     return op
 
+def hosp_create(data):
+    hospede[data['cpf']] = {'nome': data['nome'], 'cpf': data['cpf'], 'email': data['email'], 'endereco': data['endereco'], 'telefone': data['telefone']}
+
+def hosp_update(cpf):
+    cpf = input("Digite o cpf do hospede: ")
+    
+    if cpf in hospede:
+        email = input("Digite o email: ")
+        endereco = input("Digite o endereco: ")
+        telefone = input("Digite o telefone: ")
+        
+        hospede[cpf]['email'] = email,
+        hospede[cpf]['endereco'] = endereco,
+        hospede[cpf]['telefone'] = telefone 
+            
+        print('Hóspede atualizado com sucesso')
+
+    else:
+        print('Hóspede não encontrado')
+
+def hosp_read():
+    print()
+    print("---------------------------------------")
+    print("-------- Relatório de Hóspedes --------")
+    print("---------------------------------------")
+    
+    for key, value in hospede.items():
+        print("Nome: ", value['nome'], "\tCPF: ", value['cpf'], "\tTelefone: ", value['telefone'], "\tEmail: ", value['email'], "\tEndereço: ", value['endereco'])
+
+def hosp_search():
+    cpf = input('Digite o cpf do Hóspede: ')
+    name = input('Digite o nome do Hóspede: ')
+
+    for key, value in hospede.items():
+        if key == cpf or value['nome'] == name:
+            print("Nome: ", value['nome'], "\tCPF: ", value['cpf'], "\tTelefone: ", value['telefone'], "\tEmail: ", value['email'], "\tEndereço: ", value['endereco'])
+            return
+
+    print('Hóspede não encontrado')
+
+def hosp_delete(cpf):
+    del hospede[cpf]
+
+def modulo_hospede():
+    op = menu_hospede()
+
+    while op != '0':
+        if op == '1':
+            nome = input("Digite o nome do hospede: ")
+            cpf = input("Digite o cpf do hospede: ")
+            email = input("Digite o email do hospede: ")
+            endereco = input("Digite o endereco do hospede: ")
+            telefone = input("Digite o telefone do hospede: ")
+
+            data = {
+                "nome": nome,
+                "cpf": cpf,
+                "email": email,
+                "endereco": endereco,
+                "telefone": telefone
+            }
+            
+            hosp_create(data)
+
+        elif op == '2':
+            cpf = input("Digite o CPF do hospede: ")
+            hosp_update(cpf)
+
+        elif op == '3':
+            hosp_read()
+
+        elif op == '4':
+            hosp_search()
+            
+        elif op == '5':
+            cpf = input('Digite o cpf do hospede: ')
+            hosp_delete(cpf)
+
+        else:
+            print('\nSeleção inválida') 
+
+        print()
+        input('Tecle ENTER para continuar')
+
+        op = menu_hospede()
+
 def func_create(data):
     funcionario[data['cpf']] = {'nome': data['nome'], 'cpf': data['cpf'], 'salario': data['salario'], 'ocupacao': data['ocupacao']}
 
+def func_read():
+    print()
+    print("-------------------------------------------")
+    print("-------- Relatório de Funcionarios --------")
+    print("-------------------------------------------")
+    
+    for key, value in funcionario.items():
+        print("Nome: ", value['nome'], "\tCpf: ", value['cpf'], "\tSalario: ", value['salario'], "\tOcupacao: ", value['ocupacao'])
+
 def func_search():
-    pass
+    cpf = input('Digite o cpf do Funcionario: ')
+    name = input('Digite o nome do Funcionario: ')
+
+    for key, value in funcionario.items():
+        if key == cpf or value['nome'] == name:
+            print("Nome: ", value['nome'], "\tCpf: ", value['cpf'], "\tSalario: ", value['salario'], "\tOcupacao: ", value['ocupacao'])
+            return
+
+    print('Funcionario não encontrado')
 
 def func_update(cpf):
     if cpf in funcionario:
@@ -131,10 +290,12 @@ def modulo_funcionario():
             func_update(cpf)
 
         elif op == '3':
-            print(funcionario)
-            #func_search()
-            
+            func_read()
+
         elif op == '4':
+            func_search()
+            
+        elif op == '5':
             cpf = input('Digite o cpf do Funcionario: ')
             func_delete(cpf)
 
@@ -150,8 +311,7 @@ op = menu_main()
 
 while op != '0':
     if op == '1':
-        #modulo_hospede()
-        print("a")
+        modulo_hospede()
 
     elif op == '2':
         modulo_funcionario()
